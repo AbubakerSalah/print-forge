696d92a7adabcc13fa3681e31f6235733aee22cf
Abubaker <Abubakersalah4@gmail.com>
2025-04-28 02:17:25 +0200

    commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <header>
+      <nav className="flex justify-between items-center py-6 px-8  text-gray-800">
+        <Link href="/">
+          <img
+            src="./printforge-logo.svg"
+            alt=""
+            className="w-[200px] h-auto hidden md:block"
+          />
+          {/*Mobile logo*/}
+          <img
+            src="./printforge-logo-icon.svg"
+            alt=""
+            className="w-[40px] h-auto  md:hidden"
+          />
+        </Link>
+
+        <ul className="flex space-x-4">
+          <li className="hover:text-orange-400 hover:border-b transition duration-200">
+            <Link href="/3d-modal">3D Models</Link>
+          </li>
+          <li className="hover:text-orange-400 hover:border-b transition duration-200">
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import type  { RootLayoutProps } from "@app/types";
+import type { RootLayoutProps } from "@app/types";
 import { Albert_Sans, Montserrat_Alternates } from "next/font/google";
-import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 const albertSans = Albert_Sans({
   subsets: ["latin"],
@@ -14,40 +14,13 @@ const montserratAlternates = Montserrat_Alternates({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-montserrat-alternates",
 });
-export default function RootLayout({
-  children,
-}: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body
         className={`${albertSans.className} ${montserratAlternates.variable}`}
       >
-        <header>
-          <nav className="flex justify-between items-center py-6 px-8  text-gray-800">
-            <Link href="/">
-              <img
-                src="./printforge-logo.svg"
-                alt=""
-                className="w-[200px] h-auto hidden md:block"
-              />
-              {/*Mobile logo*/}
-              <img
-                src="./printforge-logo-icon.svg"
-                alt=""
-                className="w-[40px] h-auto  md:hidden"
-              />
-            </Link>
-
-            <ul className="flex space-x-4">
-              <li className="hover:text-orange-400 hover:border-b transition duration-200">
-                <Link href="/3d-modal">3D Models</Link>
-              </li>
-              <li className="hover:text-orange-400 hover:border-b transition duration-200">
-                <Link href="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
+        <Navbar />
         {children}
       </body>
     </html>
